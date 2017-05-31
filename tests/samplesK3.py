e8038033bc2d22f2970dba4a50ff7cf3ce26d191
spookyuser <spookyUnknownUser@users.noreply.github.com>
2017-05-31 20:29:54 +0200

    Kindle 3 test samples

---
+++ tests/samplesK3.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+BOOKMARK = """
+Foundation and Empire (Isaac Asimov)
+- Bookmark on Page 217 | Loc. 3313  | Added on Saturday, July 09, 2016, 10:45 PM
+
+
+
+"""
+
+HIGHLIGHT = """
+The Three-Body Problem (Cixin Liu)
+- Highlight on Page 42 | Loc. 631-32  | Added on Monday, May 15, 2017, 11:14 PM
+
+Ye saw the words TRANSMISSION MAIN CONTROL ROOM"""
+NOTE ="""
+Foundation and Empire (Isaac Asimov)
+- Note on Page 106 | Loc. 1614  | Added on Sunday, June 19, 2016, 03:49 PM
+
+k"""
+
+
+RAW = """
+The Three-Body Problem (Cixin Liu)
+- Highlight on Page 39 | Loc. 597-98  | Added on Monday, May 15, 2017, 11:09 PM
+
+By the time they landed, it was dusk.
+==========
+The Three-Body Problem (Cixin Liu)
+- Highlight on Page 42 | Loc. 631-32  | Added on Monday, May 15, 2017, 11:14 PM
+
+Ye saw the words TRANSMISSION MAIN CONTROL ROOM
+==========
+Foundation and Empire (Isaac Asimov)
+- Note on Page 106 | Loc. 1614  | Added on Sunday, June 19, 2016, 03:49 PM
+
+k
+==========
+Foundation and Empire (Isaac Asimov)
+- Bookmark on Page 217 | Loc. 3313  | Added on Saturday, July 09, 2016, 10:45 PM
+
+
+==========
+"""
